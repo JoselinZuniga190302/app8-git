c8c2af9b099cd5d7079275c160d4cdc5b95d6952
JoselinZuniga190302 <zunigajoselin128@gmail.com>
2019-05-20 14:42:00 -0500

    8

--- js/acciones.js
+++ js/acciones.js
@@ -1,10 +1,10 @@
 //JavaScript Document
 
 $(document).ready(function(e){
-	document.addEventListener("deviceready",onDeviceready,false);
-	
+	document.addEventListener("deviceready",onDeviceReady,false);
+		
 });
-
+	
 	function onDeviceReady(){
 		
 		$('#posicion').on('click',function(){
@@ -14,20 +14,19 @@ $(document).ready(function(e){
 	
 	function getPosition(){
 		var options={
-			enableHighAccuracy :true,
-			maximumAge:3600000
+			enableHighAccuracy : true,
+			maximunAge: 3600000
 		}
 		
-		var watchID=navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+		var watchID=navigator.geolocation.getCurrentPosition(onSuccess, onError,options);
 		
 		function onSuccess(position){
 			
-			alert('Latitude:'  +position.coords.latitude  +'\n'+
-			'Longitude:'  +position.coords.longitude  +'\n');
+			alert('Latitude: '     + position.coords.latitude     + '\n' + 
+			      'Longitude: '     + position.coords.latitude     + '\n');
 		};
 		
 		function onError(error){
-			alert('code:'  +error.code  +'\n'+'message:'+error.message+'\n');
+			alert('code: '    + error.code   + '\n' + 'message: '+ error.message + '\n');
 		}
-	}
-	
+	}	
